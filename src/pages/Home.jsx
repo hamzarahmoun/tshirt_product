89796d3ddc85c0a12c5325ac4772526859189a1d
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-12 12:00:57 +0100

    home v1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { motion, AnimatePresence } from 'framer-motion';
+const Home = () => {
+
+  return (
+    <AnimatePresence>
+      
+        <motion.section className="home" >
+          <motion.header >
+            <img 
+              src='./threejs.png'
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" >
+            <motion.div >
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div
+              
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+              Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+              </p>
+
+             
+            </motion.div>
+          </motion.div>
+        </motion.section>
+    </AnimatePresence>
+  )
+}
+
+export default Home
