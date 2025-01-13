2a80c071c237450cc48d69cf113da8deec725751
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-13 10:31:05 +0100

    add tshirt in the right side

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -41,6 +41,7 @@ const Shirt = () => {
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
+            
             depthTest={false}
             depthWrite={true}
           />
