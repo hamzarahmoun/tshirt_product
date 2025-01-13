d993255e904ebd620c5dee8b986f7875fde6e819
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-13 10:28:51 +0100

    add tshirt in the right side

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,6 +41,7 @@ const Home = () => {
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
+              
              
             </motion.div>
           </motion.div>
