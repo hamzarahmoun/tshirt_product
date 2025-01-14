793b962729e8e4b959c97a029808d16b9efbf774
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-14 09:29:29 +0100

    add go back buttom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home"
 import Canvas from './canvas/index';
+import Customizer from "./pages/Customizer";
 function App() {
   return (
     <main className="app transition-all ease-in">
       <Home />
       <Canvas />
+      <Customizer/>
     </main>
   )
 }
--- src/config/motion.js
+++ src/config/motion.js
@@ -50,3 +50,18 @@ export const slideAnimation = (direction) => {
       delayChildren: 0.2,
     },
   };
+
+  export const fadeAnimation = {
+    initial: {
+      opacity: 0,
+      transition: { ...transition, delay: 0.5 },
+    },
+    animate: {
+      opacity: 1,
+      transition: { ...transition, delay: 0 },
+    },
+    exit: {
+      opacity: 0,
+      transition: { ...transition, delay: 0 },
+    },
+  };
