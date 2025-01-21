b5d295d7b3e8c9dc0b77c6fcee88d4b92049f620
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-21 13:43:58 +0100

    update

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -6,12 +6,11 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
-    <div className="canvas-container">
       <Canvas
-        shadows
-        camera={{ position: [0, 0, 2], fov: 25 }}
-        gl={{ preserveDrawingBuffer: true }}
-        className="w-full h-full transition-all ease-in"
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
       >
         <ambientLight intensity={0.5} />
         <Environment preset="city" />
@@ -23,7 +22,6 @@ const CanvasModel = () => {
         </CameraRig>
 
       </Canvas>
-    </div>
   )
 }
 
